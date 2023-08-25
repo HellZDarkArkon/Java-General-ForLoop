cc829eabca3668becaf1afd42cf143bcb75fe4e9
HellZDarkArkon <maximumwerewolf@gmail.com>
2023-08-25 09:53:59 -0400

    Set up a For Loop to iterate through i, performing a logic routine for every iteration.

---
+++ com.github.hellzdarkarkon/Java/general/forloop/ForLoop.java
@@ -0,0 +1,9 @@
+package Java.general.forloop;
+
+public class ForLoop {
+    public static void main(String[] args) {
+        for (int i = 0; i < 64; i++) {
+            System.out.println("Hello, " + (i+1));
+        }
+    }
+}
